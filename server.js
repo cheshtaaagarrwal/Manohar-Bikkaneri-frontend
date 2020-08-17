dbde9af46d6a019932e02fd2f1496a3ff4191529
cheshtha agarwal <cheshthaagarwal@yahoo.in>
2020-08-17 17:19:43 +0530

    contact

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get("/about",function(request, response){
     response.sendFile(__dirname  + "/AboutUs/about.html");
 
 });
+app.get("/contact",function(request, response){
+    response.sendFile(__dirname  + "/contactus/contact.html");
+
+});
 app.listen(3000,function(){
       console.log("Server started at 3000");
 });
