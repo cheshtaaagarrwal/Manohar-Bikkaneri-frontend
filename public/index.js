a413a957eff1a20b9281f3b584c1f616fb1b5788
cheshtha agarwal <cheshthaagarwal@yahoo.in>
2020-08-18 10:14:06 +0530

    image

--- public/index.js
+++ public/index.js
@@ -23,12 +23,12 @@ var rootRef=firebase.database().ref().child("products");
       var name=childSnap.child("name").val();
       var price=childSnap.child("price").val();
       var type=childSnap.child("type").val();
-      // var image=childSnap.child("image").val();
+       var image=childSnap.child("image").val();
     
       if(type=="cake")
       {
           $("#cakes").append(
-            "<div class='col-md-3' style='display:inline-block'><div class='card '  ><img src='' class='card-img-top' alt='image'><div class='card-body' >"
+            "<div class='col-md-3' style='display:inline-block'><div class='card '  ><img src='"+image+"' class='card-img-top' alt='image'><div class='card-body' >"
             +"<h5 >"+name+
             "</h5>"+"<p class='card-text' style='display:inline;'>"+price+"</p>"+"</div></div></div>");
 
