0469cb818dcd5df6df1506d41ca2cbd85bac46a6
cheshtha agarwal <cheshthaagarwal@yahoo.in>
2020-08-25 01:15:38 +0530

    cart

--- public/index.js
+++ public/index.js
@@ -209,6 +209,110 @@ var rootRef=firebase.database().ref().child("products");
       localStorage.setItem("totalCost",product.price);
      }
  }
+ function displayCart() {
+  let cartItems = localStorage.getItem('productsincart');
+  cartItems = JSON.parse(cartItems);
+console.log(cartItems);
+  let cart = localStorage.getItem("totalCost");
+  cart = parseInt(cart);
+
+  let productContainer = document.querySelector('.products');
+  
+  if( cartItems && productContainer ) {
+      productContainer.innerHTML = '';
+      Object.values(cartItems).map( (item, index) => {
+          productContainer.innerHTML += 
+          `<div class="product"><ion-icon name="close-circle"></ion-icon><img src="./images/${item.tag}.jpg" />
+              <span class="sm-hide">${item.name}</span>
+          </div>
+          <div class="price sm-hide">$${item.price},00</div>
+          <div class="quantity">
+              <ion-icon class="decrease " name="arrow-dropleft-circle"></ion-icon>
+                  <span>${item.inCart}</span>
+              <ion-icon class="increase" name="arrow-dropright-circle"></ion-icon>   
+          </div>
+          <div class="total">$${item.inCart * item.price},00</div>`;
+      });
+
+      productContainer.innerHTML += `
+          <div class="basketTotalContainer">
+              <h4 class="basketTotalTitle">Basket Total</h4>
+              <h4 class="basketTotal">$${cart},00</h4>
+          </div>`
+
+      deleteButtons();
+      manageQuantity();
+  }
+}
+
+function manageQuantity() {
+  let decreaseButtons = document.querySelectorAll('.decrease');
+  let increaseButtons = document.querySelectorAll('.increase');
+  let currentQuantity = 0;
+  let currentProduct = '';
+  let cartItems = localStorage.getItem('productsincart');
+  cartItems = JSON.parse(cartItems);
+
+  for(let i=0; i < increaseButtons.length; i++) {
+      decreaseButtons[i].addEventListener('click', () => {
+          console.log(cartItems);
+          currentQuantity = decreaseButtons[i].parentElement.querySelector('span').textContent;
+          console.log(currentQuantity);
+          currentProduct = decreaseButtons[i].parentElement.previousElementSibling.previousElementSibling.querySelector('span').textContent.toLocaleLowerCase().replace(/ /g,'').trim();
+          console.log(currentProduct);
+
+          if( cartItems[currentProduct].inCart > 1 ) {
+              cartItems[currentProduct].inCart -= 1;
+              cartNumbers(cartItems[currentProduct], "decrease");
+              totalCost(cartItems[currentProduct], "decrease");
+              localStorage.setItem('productsincart', JSON.stringify(cartItems));
+              displayCart();
+          }
+      });
+
+      increaseButtons[i].addEventListener('click', () => {
+          console.log(cartItems);
+          currentQuantity = increaseButtons[i].parentElement.querySelector('span').textContent;
+          console.log(currentQuantity);
+          currentProduct = increaseButtons[i].parentElement.previousElementSibling.previousElementSibling.querySelector('span').textContent.toLocaleLowerCase().replace(/ /g,'').trim();
+          console.log(currentProduct);
+
+          cartItems[currentProduct].inCart += 1;
+          cartNumbers(cartItems[currentProduct]);
+          totalCost(cartItems[currentProduct]);
+          localStorage.setItem('productsincart', JSON.stringify(cartItems));
+          displayCart();
+      });
+  }
+}
+
+function deleteButtons() {
+  let deleteButtons = document.querySelectorAll('.product ion-icon');
+  let productNumbers = localStorage.getItem('cartnumbers');
+  let cartCost = localStorage.getItem("totalCost");
+  let cartItems = localStorage.getItem('productsincart');
+  cartItems = JSON.parse(cartItems);
+  let productName;
+  console.log(cartItems);
+
+  for(let i=0; i < deleteButtons.length; i++) {
+      deleteButtons[i].addEventListener('click', () => {
+          productName = deleteButtons[i].parentElement.textContent.toLocaleLowerCase().replace(/ /g,'').trim();
+         
+          localStorage.setItem('cartnumbers', productNumbers - cartItems[productName].inCart);
+          localStorage.setItem('totalCost', cartCost - ( cartItems[productName].price * cartItems[productName].inCart));
+
+          delete cartItems[productName];
+          localStorage.setItem('productsincart', JSON.stringify(cartItems));
+
+          displayCart();
+          onLoadCartNumbers();
+      })
+  }
+}
+
+onLoadCartNumbers();
+displayCart();
 
 // contactus
 var firstname,lastname,email,comment;
@@ -472,7 +576,7 @@ $("#submit").click(function(){
 
 // displayCart();
 
-onLoadCartNumbers();
+
 
 
 
