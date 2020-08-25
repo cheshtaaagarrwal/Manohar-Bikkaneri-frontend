5f2c689a8a3238a83f46de47d527e308943749b9
cheshtha agarwal <cheshthaagarwal@yahoo.in>
2020-08-25 16:16:57 +0530

    cart css

--- public/index.js
+++ public/index.js
@@ -239,22 +239,22 @@ console.log(cartItems);
       productContainer.innerHTML = '';
       Object.values(cartItems).map( (item, index) => {
           productContainer.innerHTML += 
-          `<div class="product"><ion-icon name="close-circle"></ion-icon><img src="./images/${item.tag}.jpg" />
-              <span class="sm-hide">${item.name}</span>
+          `<div class="product col-md-4"  style="text-align: center;"><ion-icon name="close-circle"></ion-icon><img src="./images/${item.tag}.jpg" />
+              <span class="sm-hide ">${item.name}</span>
           </div>
-          <div class="price sm-hide">Rs ${item.price},00</div>
-          <div class="quantity">
-              <ion-icon class="decrease " name="arrow-dropleft-circle"></ion-icon>
+          <div class="price sm-hide col-md-2"  style="text-align: center;"><i class="fas fa-rupee-sign">  </i>&nbsp ${item.price}</div>
+          <div class="quantity col-md-4" style="text-align: center;align-items: center;">
+              <ion-icon class="decrease  " name="arrow-dropleft-circle"></ion-icon>
                   <span>${item.inCart}</span>
               <ion-icon class="increase" name="arrow-dropright-circle"></ion-icon>   
           </div>
-          <div class="total">Rs${item.inCart * item.price}</div>`;
+          <div class="total md-col-2" style="text-align: center;align-items: center;"><i class="fas fa-rupee-sign"> </i>&nbsp ${item.inCart * item.price}</div>`;
       });
 
       productContainer.innerHTML += `
           <div class="basketTotalContainer">
               <h4 class="basketTotalTitle">Basket Total</h4>
-              <h4 class="basketTotal">Rs ${cart}</h4>
+              <h4 class="basketTotal"><i class="fas fa-rupee-sign"></i>&nbsp${cart}</h4>
           </div>`
 
       deleteButtons();
